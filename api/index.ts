1d7862815fa25a023410317c6f9e5243cde19d52
Amanda Carbone <amanda.elyce.carbone@gmail.com>
2022-03-31 21:32:52 -0400

    auth routes added

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import pg from 'pg-promise';
+import routes from "./routes/authRoutes";
 
 const app = express();
 
@@ -10,11 +11,12 @@ export const db = pg()({
     host: 'localhost',
     port: 5432,
     user: 'postgres',
-    password: '',
-    database: ''
+    password: '3YgJ4#zyVQ%TozKq',
+    database: 'finalproject'
 });
 
 app.use(cors());
 app.use(express.json());
+app.use('/', routes);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}.`));
---
+++ api/routes/authRoutes.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import { db } from "../index";
+import Joi from "joi";
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+
+const routes = express.Router();
+
+const schema = Joi.object({
+    first_name: Joi.string().min(1).max(500).required(),
+    last_name: Joi.string().min(1).max(500).required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'edu']} }).required(),
+    password: Joi.string().required().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'))
+});
+
+routes.post('/signup', (req, res) => {
+
+    const valid = schema.validate(req.body);
+
+    if(valid.error) {
+        return res.status(400).send(valid.error);
+    }
+
+    const hash = bcrypt.hashSync(req.body.password, saltRounds);
+
+    const newUser = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: hash
+    }
+
+    db.oneOrNone('SELECT id, email from users where email = $(email)', { email: req.body.email })
+    .then(user => {
+        if(user) {
+            return res.status(400).send("An account associated with this email already exists.")
+        }
+
+        db.one('INSERT INTO users (first_name, last_name, email, password) VALUES ($(first_name), $(last_name), $(email), $(password)) RETURNING id, first_name, last_name, email', newUser)
+        .then(id => {
+            return db.oneOrNone('SELECT id, first_name, last_name, email from users where id = $(id)', { id: id.id })
+            .then(user => res.status(201).json(user))
+            .catch(error => console.log(error))
+        });
+    });
+
+});
+
+routes.post('/login', (req, res) => {
+
+    db.oneOrNone('SELECT id, email, password FROM users where email = $(email)', { email: req.body.email })
+    .then(user => {
+        if(!user) {
+            return res.status(400).send("Invalid email or password.")
+        }
+
+        console.log(user);
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(400).send("Invalid email or password.")
+        }
+        res.json(user);
+    });
+
+});
+
+export default routes;
