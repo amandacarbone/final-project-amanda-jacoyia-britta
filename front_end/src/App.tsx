6317dd4e44297c5f04969df44d92c7d286982f25
brittany <brioden88@gmail.com>
2022-04-03 11:32:28 -0400

    Added home, search, reaults page and meal interface

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Homepage } from './components/HomePage';
+
 
 function App() {
   return (
     <div className="App">
+
+      <Homepage></Homepage>
 
     </div>
   );
--- front_end/src/index.tsx
+++ front_end/src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import { Homepage } from './components/HomePage';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    <Routes>
+      <Route path='/' element={Homepage}></Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
