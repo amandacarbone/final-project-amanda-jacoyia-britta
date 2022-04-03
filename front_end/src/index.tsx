896f4d9d2559b80a7a8fb46af8318886a57eb311
brittany <brioden88@gmail.com>
2022-04-03 13:39:04 -0400

    sorting file conflicts

--- front_end/src/index.tsx
+++ front_end/src/index.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css';
-import App from './App';
-import { Homepage } from './components/HomePage';
-=======
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
->>>>>>> 5d91e0d7fd726f909fc1175fea7f886d1c56e5c0
-
+import {Homepage} from './components/HomePage'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
