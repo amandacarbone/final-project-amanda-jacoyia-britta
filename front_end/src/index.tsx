cb98d852e5f875050d3d209b13939ac72e3b6719
IamCoy <jacoyiaj@yahoo.com>
2022-04-04 19:43:29 -0400

    merging

--- front_end/src/index.tsx
+++ front_end/src/index.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
 import {Homepage} from './components/HomePage'
-=======
-import { Home } from './components/Home';
-import SignUp from './components/Signup';
-import { Questions } from './components/Questions';
 import { ThoughtlessContextProvider } from './contexts/ThoughtlessContextProvider';
 import Login from './components/Login';
-import './index.css';
-
->>>>>>> 321509d9a34af7ad1526e9e9547c62c641cdcb84
+import SignUp from './components/Signup';
+import { Home } from './components/Home';
+import { Questions } from './components/Questions';
 ReactDOM.render(
   <React.StrictMode>
     <ThoughtlessContextProvider>
