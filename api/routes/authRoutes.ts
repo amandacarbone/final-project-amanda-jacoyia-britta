04b64fd27bc204fe8e4fd231cb7c286e2545fa87
brittany <brioden88@gmail.com>
2022-04-04 21:44:39 -0400

    search page code

--- api/routes/authRoutes.ts
+++ api/routes/authRoutes.ts
@@ -10,7 +10,7 @@ const authRoutes = express.Router();
 const schema = Joi.object({
     first_name: Joi.string().min(1).max(500).required(),
     last_name: Joi.string().min(1).max(500).required(),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'edu']} }).required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'edu'] } }).required(),
     password: Joi.string().required().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'))
 });
 
@@ -18,7 +18,7 @@ authRoutes.post('/signup', (req, res) => {
 
     const valid = schema.validate(req.body);
 
-    if(valid.error) {
+    if (valid.error) {
         return res.status(400).send(valid.error);
     }
 
@@ -32,35 +32,35 @@ authRoutes.post('/signup', (req, res) => {
     }
 
     db.oneOrNone('SELECT id, email from users where email = $(email)', { email: req.body.email })
-    .then(user => {
-        if(user) {
-            return res.status(400).send("An account associated with this email already exists.")
-        }
+        .then(user => {
+            if (user) {
+                return res.status(400).send("An account associated with this email already exists.")
+            }
 
-        db.one('INSERT INTO users (first_name, last_name, email, password) VALUES ($(first_name), $(last_name), $(email), $(password)) RETURNING id, first_name, last_name, email', newUser)
-        .then(id => {
-            return db.oneOrNone('SELECT id, first_name, last_name, email from users where id = $(id)', { id: id.id })
-            .then(user => res.status(201).json(user))
-            .catch(error => console.log(error))
+            db.one('INSERT INTO users (first_name, last_name, email, password) VALUES ($(first_name), $(last_name), $(email), $(password)) RETURNING id, first_name, last_name, email', newUser)
+                .then(id => {
+                    return db.oneOrNone('SELECT id, first_name, last_name, email from users where id = $(id)', { id: id.id })
+                        .then(user => res.status(201).json(user))
+                        .catch(error => console.log(error))
+                });
         });
-    });
 
 });
 
 authRoutes.post('/login', (req, res) => {
 
     db.oneOrNone('SELECT id, email, password FROM users where email = $(email)', { email: req.body.email })
-    .then(user => {
-        if(!user) {
-            return res.status(400).send("Invalid email or password.")
-        }
+        .then(user => {
+            if (!user) {
+                return res.status(400).send("Invalid email or password.")
+            }
 
-        console.log(user);
-        if (!bcrypt.compareSync(req.body.password, user.password)) {
-            return res.status(400).send("Invalid email or password.")
-        }
-        res.json(user);
-    });
+            console.log(user);
+            if (!bcrypt.compareSync(req.body.password, user.password)) {
+                return res.status(400).send("Invalid email or password.")
+            }
+            res.json(user);
+        });
 
 });
 
