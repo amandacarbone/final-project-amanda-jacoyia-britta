c66fc21a30e7334fa2090111408ba2b0933eb39f
IamCoy <jacoyiaj@yahoo.com>
2022-04-06 18:47:00 -0400

    added get by id endpoint

--- front_end/src/components/Result.tsx
+++ front_end/src/components/Result.tsx
@@ -3,7 +3,7 @@ import { Meal } from "../models/mealResponse";
 export function Result(props: { meal: Meal }) {
 
     return (
-        <div>
+        <div className="meal">
            
             {props.meal.strMeal}
             <img src={props.meal.strMealThumb} alt="meal thumb"/>
