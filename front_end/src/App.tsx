749909cd7baf997b321aba9bc72410f4c7c1c946
brittany <brioden88@gmail.com>
2022-04-07 18:46:56 -0400

    index.css file added, home page created

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,18 +1,16 @@
 
 import './App.css';
 import React, { useContext } from 'react';
-<<<<<<< HEAD
-import { Home } from './components/Home';
-=======
 import { toast } from 'react-toastify';
->>>>>>> df106167a7c5f45c4eea519373e8bacc8a30c953
+
+import { Header } from './components/Header';
 
 
 function App() {
 
   return (
     <div className="App">
-    <Home></Home>
+    <Header></Header>
     </div>
   );
 }
--- front_end/src/components/Home.tsx
+++ front_end/src/components/Home.tsx
@@ -1,19 +1,36 @@
 
-import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Meal, MealResponse } from "../models/mealResponse";
+import { getMealByArea, getMealByIngredient } from "../services/api";
+import { MealDisplay } from "./MealDisplay";
+
 
 export function Home() {
 
+
+   
+      
+
+    const [popular, setPopular] = useState<Meal[]>([])
+
+
+
+    useEffect (()=>{
+
+        getMealByArea('American').then(repsonse => setPopular(repsonse.meals))
+    } ,[])
+
     return (
-        <div>
 
 
-
-<h1>Thoughtless Meals </h1>
-
-<div className='navBar'>
-    <NavLink to='/'>Search</NavLink>
-</div>
+        <div className='homepage'>
+            
+            {popular.map((meal ) => < MealDisplay key={meal.idMeal} meal={meal}></MealDisplay>)}
+           
         </div>
+
+       
+        
     );
 
 }
