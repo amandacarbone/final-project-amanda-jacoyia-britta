c556869ecadf1b78a7f367b614809d3d936c0bf7
IamCoy <jacoyiaj@yahoo.com>
2022-04-06 22:02:29 -0400

    rendered the profile page, working on context for current user info

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,11 +1,8 @@
 
 import './App.css';
 import React, { useContext } from 'react';
-<<<<<<< HEAD
-import { Home } from './components/Home';
-=======
 import { toast } from 'react-toastify';
->>>>>>> df106167a7c5f45c4eea519373e8bacc8a30c953
+import { Home } from './components/Home';
 
 
 function App() {
