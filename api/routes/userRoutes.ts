835c65d41d97590865e97cb8e8e8958964dd40c3
IamCoy <jacoyiaj@yahoo.com>
2022-04-11 18:43:57 -0400

    PUT request added, and functioning on front-end app, added toastify to profile details component

--- api/routes/userRoutes.ts
+++ api/routes/userRoutes.ts
@@ -26,6 +26,30 @@ userRoutes.delete("/users/:id", (req, res) => {
         .then(users => res.json(users))
 })
 
+//endpoint to update user first name, last name, and email only
+userRoutes.put("/users/update/:id", (req, res) => {
+    db.many("select * from users")
+      .then((user) => {
+        let foundUser= user.find((u) => u.id === +req.params.id);
+  
+        if (!foundUser) {
+          res.status(404).json({ error: "User account does not exist" });
+        } else {
+          db.none(
+            "UPDATE users SET id=${id}, first_name=$(first_name), last_name = $(last_name), email = $(email) where id = ${id}",
+            {
+              id: +req.params.id,
+              first_name: req.body.first_name,
+              last_name: req.body.last_name,
+              email: req.body.email,
+            }
+          );
+  
+          res.send(req.body);
+        }
+      })
+  
+  });
 
 
 
--- front_end/src/services/Users.ts
+++ front_end/src/services/Users.ts
@@ -16,4 +16,15 @@ export function deleteUser(id: number) {
     return axios
     .delete(`http://localhost:3005/users/${id}`)
     .then((response) => response.data)
+};
+
+export function updateBasics(id:number, first_name: string, last_name: string, email: string) {
+    return axios
+    .put(`http://localhost:3005/users/update/${id}`, {
+        first_name: first_name,
+        last_name: last_name,
+        email: email
+      
+    })
+    .then((response) => response.data);
 }
