560a203689b24f6a9ad3d273ed9bb4a04cc1f0c4
brittany <brioden88@gmail.com>
2022-04-14 19:08:47 -0400

    card format change

--- front_end/src/components/favorites/Favorites.tsx
+++ front_end/src/components/favorites/Favorites.tsx
@@ -4,10 +4,14 @@ import { toast } from "react-toastify";
 import { ThoughtlessContext } from "../../contexts/ThoughtlessContext";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Home } from "../home/Home";
+import { SearchPage } from "../search/SearchPage";
 
 export function Favorites() {
 
     const { favoriteMeals, removeFavorite } = useContext(ThoughtlessContext);
+
+    
 
     function showRemoveButton() {
         let foundItem = favoriteMeals.find((meal) => meal.idMeal)
@@ -19,8 +23,13 @@ export function Favorites() {
     }
 
     return (
-        <div>
+        <div className='favorites'>
+
+
+           
+
             <div>
+
                 {favoriteMeals.map((meal) => (
                     <ul key={meal.idMeal}>
                         <li><img src={meal?.strMealThumb}/></li>
