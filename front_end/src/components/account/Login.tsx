df4c384224a38c69101e4656f7b690ae433287dc
IamCoy <jacoyiaj@yahoo.com>
2022-04-13 21:26:59 -0400

    dietary preferences updating from checked boxes, updated login route to return needed columns

--- front_end/src/components/account/Login.tsx
+++ front_end/src/components/account/Login.tsx
@@ -36,7 +36,6 @@ function handleSubmit(e: any) {
 
   e.preventDefault();
   login(email, password).then((data: any) => {
-    if (data) console.log(data);
     localStorage.setItem("user", JSON.stringify(data));
   });
   navigate('/home');
