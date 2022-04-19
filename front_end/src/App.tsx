99c04c7ad022012c9c96866516be40cba7448ec4
Amanda Carbone <amanda.elyce.carbone@gmail.com>
2022-04-18 22:32:24 -0400

    set up firebase

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,6 +2,21 @@
 import './App.css';
 import { useLocation } from 'react-router-dom';
 import { Header } from './components/constants/Header';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9QAl1Ubi4eRxP4akDfzPHiRsLZCG2pIg",
+  authDomain: "thoughtless-da50e.firebaseapp.com",
+  projectId: "thoughtless-da50e",
+  storageBucket: "thoughtless-da50e.appspot.com",
+  messagingSenderId: "57802830627",
+  appId: "1:57802830627:web:aafe84cdd884bf1c239296",
+  measurementId: "G-C588CKV8FH"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 function App() {
